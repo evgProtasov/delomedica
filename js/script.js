077a86819dd8ab75506a315e7a801c5094be27fe
Evgeny Protasov <j.protasow@gmail.com>
2022-07-08 13:21:41 +0300

    Buttons Nest and Prev

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n)
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n)
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("steps");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.add('none')
+    }
+    slides[slideIndex-1].classList.remove('none');
+}
